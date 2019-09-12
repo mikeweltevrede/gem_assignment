2a32ddb7bbba79181891c9ce7ed916e5891a4e9e
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2019-09-12 17:54:56 +0200

    Add import data

--- assignment_b.R
+++ assignment_b.R
@@ -1,7 +1,48 @@
 ### From pseudocode
+rm(list = ls())
 
 ## Initialising steps
+library(readxl)
+library(bazar)
 
+import_data <- function(file_location) {
+  data <- readxl::read_excel(file_location, .name_repair = "minimal")
+  colnames(data)[1] <- "Patient"
+
+  # Define the priority ordering f
+  f <- sort(data$Patient)
+
+  # Define which patient ID codes for the waiting list (maximum patient ID + 1)
+  number_of_patients <- max(f)
+  w <- number_of_patients + 1
+
+  colnames(data) <- append("Patient", as.character(1:w))
+  patient_names <- as.character(data$Patient)
+
+  # Define preference profiles; i.e. all data except for patient ID
+  preferences <- data[, -1]
+  rm(data) # Clean up; `data` is not needed in the rest of this function
+
+  # Initialise kidneys: make patients "point" to currently "favourite" kidneys
+  current_assignment <- t(preferences[, 1]) # Initialise to first choice
+  names(current_assignment) <- patient_names
+  current_assignment <- as.list(current_assignment)
+
+  # Initialise the final_assignment to 0
+  final_assignment <- t(as.matrix(rep(0, number_of_patients)))
+  names(final_assignment) <- patient_names
+  final_assignment <- as.list(final_assignment)
+
+  return(list("f" = f,
+              "w" = w,
+              "preferences" = preferences,
+              "current_assignment" = current_assignment,
+              "final_assignment" = final_assignment,
+              "assigned" = c(),
+              "available_kidneys" = c()))
+}
+
+data <- import_data(file_location = "data/dataset7.xlsx")
 
 ## Algorithm
 # Start;
@@ -10,6 +51,12 @@
 # available option;
 
 # Find all cycles C;
+cycle_finder <- function(current_assignment){
+  return(c(0,1,2,3))
+}
+
+cycles = cycle_finder(data$current_assignment)
+print(cycles)
 
 # Find the patient t with the highest priority in C with currently best
 # preference p_t^∗
@@ -47,7 +94,7 @@
     # Remove t from patient list;
     # Return to Start;
   # else
-    #   Find all cycles;
+    # Find all cycles;
     # Go to Step X
 
-# w-chains
+# w chains
